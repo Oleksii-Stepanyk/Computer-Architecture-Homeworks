f3ed998640be3ea54051c811bedf03450eab72eb
Oleksii Stepanyk <o.stepanik06@gmail.com>
2025-12-15 16:00:49 +0200

    Updated TopModule Testbench

--- Practice_9/TopModuleTb.sv
+++ Practice_9/TopModuleTb.sv
@@ -5,9 +5,11 @@ module TopModuleTb();
     logic reset;
     logic [31:0] WriteData, DataAdr, Instr;
     logic MemWrite;
+    logic irrigation_active, ventilation_active, peripheral_done;
 
     // instantiate device to be tested
-    TopModule dut(clk, reset, WriteData, DataAdr, Instr, MemWrite);
+    TopModule dut(clk, reset, WriteData, DataAdr, Instr, MemWrite,
+        irrigation_active, ventilation_active, peripheral_done);    
     // initialize test
     initial begin
         reset <= 1; #13; reset <= 0;
@@ -16,10 +18,21 @@ module TopModuleTb();
     always begin
         clk <= 1; #10; clk <= 0; #10;
     end
+    
+    // Monitor peripheral outputs
+    always @(posedge clk) begin
+        if (irrigation_active || ventilation_active || peripheral_done) begin
+            $display("Time=%0t: irrigation=%b, ventilation=%b, done=%b", 
+                     $time, irrigation_active, ventilation_active, peripheral_done);
+        end
+    end
+    
     // check results
     always @(negedge clk) begin
         if (!reset && Instr == 32'h0000006f) begin
             $display("Hit infinite JAL loop at Instr=%h, stopping", Instr);
+            $display("Final peripheral state: irrigation=%b, ventilation=%b, done=%b",
+                     irrigation_active, ventilation_active, peripheral_done);
             $stop;
         end
     end
